b539ccd1e481f17b7813b2206862ac0680287cb8
Codnixus <27430075-codnixus@users.noreply.gitlab.com>
2026-06-13 09:27:53 +0100

    Update release.nix to use github releases

--- release.nix
+++ release.nix
@@ -2,17 +2,27 @@
   stdenv,
   autoPatchelfHook,
   system,
+  pkgs,
+  rustPlatform ? pkgs.rustPlatform,
 }:
-stdenv.mkDerivation {
-  pname = "ncap";
-  version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
-  src = ./. + "/releases/${system}.tar.gz";
-  sourceRoot = ".";
-  preferLocalBuild = true;
-  nativeBuildInputs = [ autoPatchelfHook ];
-  buildInputs = [ stdenv.cc.cc.lib ];
-  installPhase = ''
-    mkdir $out
-    cp -r bin share $out/
-  '';
-}
+let
+  version = (fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+  maybeRemote = builtins.tryEval (builtins.fetchurl {
+    url = "https://github.com/hexrustox/nix-capsule/releases/download/${version}/${system}.tar.gz";
+  });
+in
+if maybeRemote.success then
+  stdenv.mkDerivation {
+    pname = "ncap";
+    inherit version;
+    src = maybeRemote.value;
+    sourceRoot = ".";
+    nativeBuildInputs = [ autoPatchelfHook ];
+    buildInputs = [ stdenv.cc.cc.lib ];
+    installPhase = ''
+      mkdir $out
+      cp -r bin share $out/
+    '';
+  }
+else
+  import ./package.nix { inherit pkgs rustPlatform; }
