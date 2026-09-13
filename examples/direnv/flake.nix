e2f72e7e02de539722f5dc5d1a8a486f236ed57b
hexrustox <231717567+hexrustox@users.noreply.github.com>
2026-09-13 11:07:38 +0100

    Add examples

---
+++ examples/direnv/flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nix-capsule.url = "path:../../";
+    nixpkgs.follows = "nix-capsule/nixpkgs";
+    flake-parts.follows = "nix-capsule/flake-parts";
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      perSystem =
+        {
+          system,
+          ...
+        }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.nix-capsule.overlays.default
+            ];
+          };
+          capsule-lib = inputs.nix-capsule.lib { inherit pkgs; };
+        in
+        {
+          apps.default = capsule-lib.app;
+          devShells = {
+            default = capsule-lib.mkShell {
+              project = "nix-capsule-example-direnv";
+              wrappers = [
+                "cowsay"
+              ];
+              postShellHook = ''
+                echo Welcome to nix capsule devshell
+              '';
+            };
+
+            container = pkgs.mkShellNoCC {
+              packages = with pkgs; [
+                cowsay
+              ];
+            };
+          };
+        };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    };
+}
---
+++ examples/simple/flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nix-capsule.url = "path:../../";
+    nixpkgs.follows = "nix-capsule/nixpkgs";
+    flake-parts.follows = "nix-capsule/flake-parts";
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      perSystem =
+        {
+          system,
+          ...
+        }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.nix-capsule.overlays.default
+            ];
+          };
+          capsule-lib = inputs.nix-capsule.lib { inherit pkgs; };
+        in
+        {
+          devShells = {
+            default = capsule-lib.mkShell {
+              project = "nix-capsule-example-simple";
+              wrappers = [
+                "cowsay"
+              ];
+              postShellHook = ''
+                echo Welcome to nix capsule devshell
+              '';
+            };
+
+            container = pkgs.mkShellNoCC {
+              packages = with pkgs; [
+                cowsay
+              ];
+            };
+          };
+        };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    };
+}
