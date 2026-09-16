d37dd92bb8afb6e4d992119676af9d6a2ba26068
hexrustox <231717567+hexrustox@users.noreply.github.com>
2026-09-17 00:04:19 +0100

    Simplify examples

--- examples/simple/flake.nix
+++ examples/simple/flake.nix
@@ -2,53 +2,42 @@
   inputs = {
     nix-capsule.url = "path:../../";
     nixpkgs.follows = "nix-capsule/nixpkgs";
-    flake-parts.follows = "nix-capsule/flake-parts";
   };
 
   outputs =
-    { flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      perSystem =
-        {
-          system,
-          ...
-        }:
-        let
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.nix-capsule.overlays.from-source
-            ];
-          };
-          capsule-lib = inputs.nix-capsule.lib { inherit pkgs; };
-        in
-        {
-          devShells = {
-            default = capsule-lib.mkShell {
-              project = "nix-capsule-example-simple";
-              image = "alpine:latest";
-              wrappers = [
-                "cowsay"
-              ];
-              postShellHook = ''
-                echo Welcome to nix capsule devshell
-              '';
-            };
-
-            container = pkgs.mkShellNoCC {
-              packages = with pkgs; [
-                cowsay
-              ];
-              shellHook = ''
-                ${capsule-lib.devShellGuard}
-              '';
-            };
-          };
+    { ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.nix-capsule.overlays.from-source
+        ];
+      };
+      capsule-lib = inputs.nix-capsule.lib { inherit pkgs; };
+    in
+    {
+      devShells.${system} = {
+        default = capsule-lib.mkShell {
+          project = "nix-capsule-example-simple";
+          image = "alpine:latest";
+          wrappers = [
+            "hello"
+          ];
+          postShellHook = ''
+            echo Welcome to nix capsule devshell
+          '';
         };
 
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+        container = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            hello
+          ];
+          shellHook = ''
+            ${capsule-lib.devShellGuard}
+          '';
+        };
+      };
+
     };
 }
