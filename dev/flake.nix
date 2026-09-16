3ad31f0c10698361c4098461b9f13f56bb63f76a
hexrustox <231717567+hexrustox@users.noreply.github.com>
2026-09-16 23:51:43 +0100

    Update nix-capsule to v0.10.2

--- dev/flake.nix
+++ dev/flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.follows = "root/nixpkgs";
     rust-overlay.follows = "root/rust-overlay";
     flake-parts.follows = "root/flake-parts";
-    nix-capsule.url = "github:hexrustox/nix-capsule?ref=v0.10.0";
+    nix-capsule.url = "github:hexrustox/nix-capsule?ref=v0.10.2";
   };
 
   outputs =
