ce7dc3337aa82a1a050f6c9e52dc4d4be5b8f936
hexrustox <231717567+hexrustox@users.noreply.github.com>
2026-09-16 23:45:40 +0100

    build(dev): move devshell into consumer flake pinning nix-capsule v0.10.0
    - Root flake no longer consumes nix-capsule; it only exposes packages
    - dev/flake.nix provides Host/Container shells via nix-capsule v0.10.0
    - .envrc now enters the devshell via `use flake ./dev`

---
+++ dev/flake.nix
@@ -0,0 +1,88 @@
+{
+  inputs = {
+    root.url = "path:../";
+    nixpkgs.follows = "root/nixpkgs";
+    rust-overlay.follows = "root/rust-overlay";
+    flake-parts.follows = "root/flake-parts";
+    nix-capsule.url = "github:hexrustox/nix-capsule?ref=v0.10.0";
+  };
+
+  outputs =
+    { self, flake-parts, ... }@inputs:
+    let
+      rustVersion = "1.95.0";
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      perSystem =
+        {
+          system,
+          ...
+        }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.nix-capsule.overlays.default
+            ];
+          };
+          capsule-lib = inputs.nix-capsule.lib { inherit pkgs; };
+        in
+        {
+          devShells = {
+            default = capsule-lib.mkShell {
+              image = "alpine:latest";
+              watchFiles = ["flake.nix" "flake.lock" "dev/flake.nix" "dev/flake.lock"];
+              devShell = "./dev#container";
+              extraOptions = [
+                "-e"
+                "NIX_PATH"
+                "-e"
+                "CARGO_HOME"
+                "-v"
+                "$CARGO_HOME:$CARGO_HOME"
+              ];
+              wrappers = [
+                "cargo"
+                "codebook-lsp"
+                "rust-analyzer"
+                "nixd"
+                "taplo"
+              ];
+              preShellHook = ''
+                export CARGO_HOME=''${CARGO_HOME:-$HOME/.cargo}
+                mkdir -p "$CARGO_HOME"
+              '';
+            };
+            container = pkgs.mkShellNoCC {
+              packages = with pkgs; [
+                cargo-deny
+                cargo-edit
+                cargo-machete
+                cargo-llvm-cov
+                clang
+                codebook
+                nixd
+                nixfmt
+                mold
+                taplo
+                (rust-bin.stable.${rustVersion}.default.override {
+                  extensions = [
+                    "rust-src"
+                    "rust-analyzer"
+                    "llvm-tools-preview"
+                  ];
+                })
+                skills
+                git
+              ];
+            };
+          };
+        };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    };
+}
--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nix-capsule.url = "github:hexrustox/nix-capsule?ref=v0.9.0";
   };
 
   outputs =
@@ -25,11 +24,7 @@
               system = prev.stdenv.targetPlatform.system;
             in
             {
-              ncap =
-                if system == "x86_64-linux" then
-                  self.packages.x86_64-linux.ncap-prebuilt
-                else
-                  self.packages.${system}.default;
+              ncap = self.packages.${system}.ncap-prebuilt;
             };
           from-source =
             final: prev:
@@ -52,81 +47,23 @@
             inherit system;
             overlays = [
               inputs.rust-overlay.overlays.default
-              inputs.nix-capsule.overlays.default
             ];
           };
-          capsule-lib = inputs.nix-capsule.lib { inherit pkgs; };
+          rust = pkgs.rust-bin.stable.${rustVersion}.default;
         in
         {
-          packages =
-            let
-              pkgs = import inputs.nixpkgs {
-                inherit system;
-                overlays = [
-                  inputs.rust-overlay.overlays.default
-                ];
+          packages = {
+            default = pkgs.callPackage ./package.nix {
+              rustPlatform = pkgs.makeRustPlatform {
+                cargo = rust;
+                rustc = rust;
               };
-              rust = pkgs.rust-bin.stable.${rustVersion}.default;
-            in
-            {
-              default = pkgs.callPackage ./package.nix {
-                rustPlatform = pkgs.makeRustPlatform {
-                  cargo = rust;
-                  rustc = rust;
-                };
-              };
-              ncap-prebuilt =
-                if system == "x86_64-linux" then
-                  pkgs.callPackage ./prebuilt.nix { }
-                else
-                  throw "ncap-prebuilt: no prebuilt artifact for ${system}; use packages.${system}.default";
             };
-
-          devShells = {
-            default = capsule-lib.mkShell {
-              extraOptions = [
-                "-e"
-                "NIX_PATH"
-                "-e"
-                "CARGO_HOME"
-                "-v"
-                "$CARGO_HOME:$CARGO_HOME"
-              ];
-              wrappers = [
-                "cargo"
-                "codebook-lsp"
-                "rust-analyzer"
-                "nixd"
-                "taplo"
-              ];
-              preShellHook = ''
-                export CARGO_HOME=''${CARGO_HOME:-$HOME/.cargo}
-                mkdir -p "$CARGO_HOME"
-              '';
-            };
-            container = pkgs.mkShellNoCC {
-              packages = with pkgs; [
-                cargo-deny
-                cargo-edit
-                cargo-machete
-                cargo-llvm-cov
-                clang
-                codebook
-                nixd
-                nixfmt
-                mold
-                taplo
-                (rust-bin.stable.${rustVersion}.default.override {
-                  extensions = [
-                    "rust-src"
-                    "rust-analyzer"
-                    "llvm-tools-preview"
-                  ];
-                })
-                skills
-                git
-              ];
-            };
+            ncap-prebuilt =
+              if system == "x86_64-linux" then
+                pkgs.callPackage ./prebuilt.nix { }
+              else
+                throw "ncap-prebuilt: no prebuilt artifact for ${system}; use packages.${system}.default";
           };
         };
 
