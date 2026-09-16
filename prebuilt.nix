36718945dd8f2f24fe2a9369e2417c04d1def4ce
hexrustox <231717567+hexrustox@users.noreply.github.com>
2026-09-16 17:56:17 +0100

    Fix release tarball hash

--- prebuilt.nix
+++ prebuilt.nix
@@ -9,7 +9,7 @@ let
   system = "x86_64-linux";
   cargoToml = fromTOML (builtins.readFile ./Cargo.toml);
   version = cargoToml.package.version;
-  hash = "sha256-fVFx5b7F8SJUI54VXbLNzneP5pPi5ZTLLNih5TI+PZ4=";
+  hash = "sha256-Uc4kz2GOc/4ScOrSFXNgDrNDcQuFccXECauCyeA4XQg=";
 in
 stdenv.mkDerivation {
   inherit version;
